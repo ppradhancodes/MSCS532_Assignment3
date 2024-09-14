42f40e36ae1651bf8d71faa7088330c2a3bd0b88
Prafulla Pradhan <ppradhan22638@ucumberlands.edu>
2024-09-13 20:21:59 -0500

    MSCS532_Assignment3: Added quickSort.py

---
+++ quickSort.py
@@ -0,0 +1,30 @@
+import random
+
+def randomized_quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = random.choice(arr)
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return randomized_quicksort(left) + middle + randomized_quicksort(right)
+
+def deterministic_quicksort(arr):
+    stack = [(0, len(arr) - 1)]
+    while stack:
+        low, high = stack.pop()
+        if low < high:
+            pi = partition(arr, low, high)
+            stack.append((low, pi - 1))
+            stack.append((pi + 1, high))
+    return arr
+
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
